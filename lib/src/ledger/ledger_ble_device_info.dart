66469ff9dffe2417c70ae7287c9d76d2fe7157a4
Konstantin Ullrich <konstantinullrich12@gmail.com>
2024-08-05 13:20:11 +0200

    Add Ledger Flex Support

--- lib/src/ledger/ledger_ble_device_info.dart
+++ lib/src/ledger/ledger_ble_device_info.dart
@@ -8,6 +8,11 @@ enum LedgerBleDeviceInfo {
     serviceId: '13D63400-2C97-6004-0000-4C6564676572',
     writeCharacteristicKey: '13d63400-2c97-6004-0002-4c6564676572',
     notifyCharacteristicKey: '13D63400-2C97-6004-0001-4C6564676572',
+  ),
+  flex(
+    serviceId: '13D63400-2C97-3004-0000-4C6564676572',
+    writeCharacteristicKey: '13d63400-2c97-3004-0002-4c6564676572',
+    notifyCharacteristicKey: '13D63400-2C97-3004-0001-4C6564676572',
   );
 
   const LedgerBleDeviceInfo({
